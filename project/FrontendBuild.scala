fd186e39a3b2a3bc8ee5297c3be95675d94bcbab
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2018-02-06 15:33:14 +0000

    SAR-1371: WIP ip white listing and unit tests

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -11,20 +11,31 @@ object FrontendBuild extends Build with MicroService {
 
   override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
 
+  private val playWhitelistFilterVersion = "1.1.0"
+
+  private val hmrcTestVersion = "3.0.0"
+  private val scalaTestVersion = "3.0.1"
+  private val scalaTestPlusVersion = "2.0.0"
+  private val pegdownVersion = "1.6.0"
+  private val jsoupVersion =  "1.8.1"
+  private val mockitoVersion = "2.7.6"
+
   val compile = Seq(
     "uk.gov.hmrc" %% "govuk-template" % "5.18.0",
     "uk.gov.hmrc" %% "play-ui" % "7.13.0",
-
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.3.0"
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.3.0",
+    "uk.gov.hmrc" %% "play-whitelist-filter" % playWhitelistFilterVersion
   )
 
   def test(scope: String = "test") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
-    "org.scalatest" %% "scalatest" % "2.2.6" % scope,
-    "org.pegdown" % "pegdown" % "1.6.0" % scope,
-    "org.jsoup" % "jsoup" % "1.8.1" % scope,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+    "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
+    "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusVersion % scope,
+    "org.pegdown" % "pegdown" % pegdownVersion % scope,
+    "org.jsoup" % "jsoup" % jsoupVersion % scope,
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+    "org.mockito" % "mockito-core" % mockitoVersion % scope
   )
 
 }
