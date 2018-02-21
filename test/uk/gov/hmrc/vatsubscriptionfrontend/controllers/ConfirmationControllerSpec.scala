3b9eeb8750f2a09de947970c0285d3d005dec2ea
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2018-02-21 17:42:54 +0000

    SAR-1513: added controller for confirmation

---
+++ test/uk/gov/hmrc/vatsubscriptionfrontend/controllers/ConfirmationControllerSpec.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsubscriptionfrontend.controllers
+
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.http.Status
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.auth.core.retrieve.EmptyRetrieval
+import uk.gov.hmrc.play.test.UnitSpec
+import uk.gov.hmrc.vatsubscriptionfrontend.config.mocks.MockControllerComponents
+
+import scala.concurrent.Future
+
+class ConfirmationControllerSpec extends UnitSpec with GuiceOneAppPerSuite with MockControllerComponents {
+
+  object TestConfirmationController extends ConfirmationController(mockControllerComponents)
+
+  lazy val testGetRequest = FakeRequest("GET", "/information-received")
+
+  lazy val testPostRequest: FakeRequest[AnyContentAsEmpty.type] =
+    FakeRequest("POST", "/information-received")
+
+  "Calling the show action of the Confirmation controller" should {
+    "show the Confirmation page" in {
+      mockAuthorise(retrievals = EmptyRetrieval)(Future.successful(Unit))
+      val request = testGetRequest
+
+      val result = TestConfirmationController.show(request)
+      status(result) shouldBe Status.OK
+      contentType(result) shouldBe Some("text/html")
+      charset(result) shouldBe Some("utf-8")
+    }
+  }
+
+  "Calling the submit action of the Confirmation controller" should {
+    // todo
+    "return not implemented" in {
+      mockAuthorise(retrievals = EmptyRetrieval)(Future.successful(Unit))
+
+      val result = TestConfirmationController.submit(testPostRequest)
+      status(result) shouldBe Status.NOT_IMPLEMENTED
+    }
+  }
+
+}
