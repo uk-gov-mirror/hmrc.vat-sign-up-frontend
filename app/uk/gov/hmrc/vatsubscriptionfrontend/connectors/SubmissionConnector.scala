4b6fd4eb6855f087f19e20d2f20945a7fcbf48a7
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2018-03-01 10:53:01 +0000

    SAR-1567: added connector to call submission

---
+++ app/uk/gov/hmrc/vatsubscriptionfrontend/connectors/SubmissionConnector.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsubscriptionfrontend.connectors
+
+import javax.inject.{Inject, Singleton}
+
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
+import uk.gov.hmrc.vatsubscriptionfrontend.config.AppConfig
+import uk.gov.hmrc.vatsubscriptionfrontend.httpparsers.SubmissionHttpParser._
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class SubmissionConnector @Inject()(val http: HttpClient,
+                                    val applicationConfig: AppConfig) {
+
+  private def url(vatNumber: String): String = s"${applicationConfig.protectedMicroServiceUrl}/vat-number/$vatNumber/submit"
+
+  def submit(vatNumber: String)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[SubmissionResponse] =
+    http.POSTEmpty[SubmissionResponse](url(vatNumber))
+
+}
