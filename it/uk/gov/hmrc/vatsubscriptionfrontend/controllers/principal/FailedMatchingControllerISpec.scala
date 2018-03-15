16b58c1d953a7e90ddb74aaacc6dcf7d8a91fc3f
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2018-03-15 11:16:25 +0000

    SAR-1716: component tests for failed matching

---
+++ it/uk/gov/hmrc/vatsubscriptionfrontend/controllers/principal/FailedMatchingControllerISpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsubscriptionfrontend.controllers.principal
+
+import play.api.http.Status._
+import uk.gov.hmrc.vatsubscriptionfrontend.helpers.servicemocks.AuthStub._
+import uk.gov.hmrc.vatsubscriptionfrontend.helpers.{ComponentSpecBase, CustomMatchers}
+
+class FailedMatchingControllerISpec extends ComponentSpecBase with CustomMatchers {
+
+  "GET /error/incorrect-details" should {
+    "return an NOT_IMPLEMENTED" in {
+      stubAuth(OK, successfulAuthResponse())
+
+      val res = get("/error/incorrect-details")
+
+      //todo
+      res should have(
+        httpStatus(NOT_IMPLEMENTED)
+//        httpStatus(OK)
+      )
+    }
+  }
+
+  "POST /error/incorrect-details" should {
+    "redirect to the capture email address page" in {
+      stubAuth(OK, successfulAuthResponse())
+
+      val res = post("/error/incorrect-details")()
+
+      res should have(
+        httpStatus(SEE_OTHER),
+        redirectUri(routes.CaptureYourDetailsController.show().url)
+      )
+    }
+  }
+
+}
