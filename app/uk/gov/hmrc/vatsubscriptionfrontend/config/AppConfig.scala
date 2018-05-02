84728ef901be7a736e3e22d4097bb9b2e5c3c833
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2018-05-02 17:52:08 +0100

    SAR-1942: test only route to call tax enrolment as the issuer

--- app/uk/gov/hmrc/vatsubscriptionfrontend/config/AppConfig.scala
+++ app/uk/gov/hmrc/vatsubscriptionfrontend/config/AppConfig.scala
@@ -93,6 +93,8 @@ class AppConfig @Inject()(val runModeConfiguration: Configuration, environment: 
 
   lazy val betaFeedbackUnauthenticatedUrl = s"$contactFrontendUrl/contact/beta-feedback-unauthenticated?service=$contactFormServiceIdentifier"
 
+  lazy val taxEnrolmentUrl = baseUrl("tax-enrolment")
+
   /*
   *  This checks to see if the testOnlyDoNotUseInAppConf route is set in configuration instead of the default prod.Routes
   *  This flag can be used by the application to check if the test only routes are enabled. i.e. this flag can be used to
