b32285fe6a7d8760d8774631bd514f558a1a6f0f
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2018-06-08 11:30:49 +0100

    SAR-2128: WIP vat number checksum validation

---
+++ test/uk/gov/hmrc/vatsignupfrontend/utils/VatNumberChecksumValidationSpec.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.utils
+
+import org.scalatest.mockito.MockitoSugar
+import uk.gov.hmrc.play.test.UnitSpec
+
+
+class VatNumberChecksumValidationSpec extends UnitSpec with MockitoSugar {
+
+  "VatNumberChecksumValidation.isValid" should {
+    "return true if the checksum is correct for a mod 97 number" in {
+       val testValue = "011000084"
+      VatNumberChecksumValidation.isValid(testValue) shouldBe true
+    }
+
+    "return true if the checksum is correct for a mod 9755 number" in {
+      val testValue = "011000029"
+      VatNumberChecksumValidation.isValid(testValue) shouldBe true
+    }
+
+    "return false if the checksum is incorrect" in {
+      val testValue = "999999999"
+      VatNumberChecksumValidation.isValid(testValue) shouldBe false
+    }
+  }
+
+}
