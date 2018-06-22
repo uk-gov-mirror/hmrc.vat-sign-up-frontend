bd9af9915be3107b282808b8abbe7d10cfc78f37
michael-jeacock <michael.jeacock@digital.hmrc.gov.uk>
2018-06-22 16:38:17 +0100

    SAR-2218: Capture UTR form spec tests

---
+++ test/uk/gov/hmrc/vatsignupfrontend/forms/CompanyUtrFormSpec.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.forms
+
+import org.scalatest.Matchers._
+import org.scalatestplus.play.PlaySpec
+import play.api.data.FormError
+import uk.gov.hmrc.vatsignupfrontend.helpers.TestConstantsGenerator
+import uk.gov.hmrc.vatsignupfrontend.helpers.TestConstantsGenerator._
+
+class CompanyUtrFormSpec extends PlaySpec {
+
+  import uk.gov.hmrc.vatsignupfrontend.forms.CompanyUtrForm._
+
+  "The companyUtrForm" should {
+
+    val errorKey = "error.invalid_company_utr"
+
+    "validate that data containing 10 digits passes" in {
+      val testUtr = TestConstantsGenerator.randomUTRNumeric()
+      val actual = companyUtrForm.bind(Map(companyUtr -> testUtr)).value
+      actual shouldBe Some(testUtr)
+    }
+
+    "validate that no data has been entered" in {
+      val formWithError = companyUtrForm.bind(Map(companyUtr -> ""))
+      formWithError.errors should contain(FormError(companyUtr, errorKey))
+    }
+
+    "validate that data containing incorrect format of non numeric data fails" in {
+      val formWithError = companyUtrForm.bind(Map(companyUtr -> "123A456 A"))
+      formWithError.errors should contain(FormError(companyUtr, errorKey))
+    }
+
+    "validate that data containing more than 10 digits fails" in {
+      val formWithError = companyUtrForm.bind(Map(companyUtr -> "123 456 789 10"))
+      formWithError.errors should contain(FormError(companyUtr, errorKey))
+    }
+
+    "validate that data containing fewer than 10 digits but greater than 0 fails" in {
+      val testUtr = "1"
+      val formWithError = companyUtrForm.bind(Map(companyUtr -> testUtr))
+      formWithError.errors should contain(FormError(companyUtr, errorKey))
+    }
+
+  }
+
+}
