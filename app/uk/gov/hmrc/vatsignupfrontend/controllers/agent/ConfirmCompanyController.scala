f44e57299aa04709172ebde6c2cddae50b1e7d18
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2018-07-23 12:22:50 +0100

    SAR-2291: controller and routes for agent confirm company

---
+++ app/uk/gov/hmrc/vatsignupfrontend/controllers/agent/ConfirmCompanyController.scala
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.controllers.agent
+
+import javax.inject.{Inject, Singleton}
+
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.http.InternalServerException
+import uk.gov.hmrc.vatsignupfrontend.config.ControllerComponents
+import uk.gov.hmrc.vatsignupfrontend.config.auth.AgentEnrolmentPredicate
+import uk.gov.hmrc.vatsignupfrontend.controllers.AuthenticatedController
+import uk.gov.hmrc.vatsignupfrontend.services.StoreCompanyNumberService
+import uk.gov.hmrc.vatsignupfrontend.views.html.agent.confirm_company
+import uk.gov.hmrc.vatsignupfrontend.SessionKeys._
+import uk.gov.hmrc.vatsignupfrontend.config.featureswitch.CompanyNameJourney
+
+import scala.concurrent.Future
+
+@Singleton
+class ConfirmCompanyController @Inject()(val controllerComponents: ControllerComponents,
+                                         val storeCompanyNumberService: StoreCompanyNumberService)
+  extends AuthenticatedController(AgentEnrolmentPredicate, featureSwitches = Set(CompanyNameJourney)) {
+
+  val show: Action[AnyContent] = Action.async { implicit request =>
+    authorised() {
+      val optCompanyName = request.session.get(companyNameKey).filter(_.nonEmpty)
+
+      Future.successful(
+        optCompanyName match {
+          case Some(companyName) =>
+            val changeLink = routes.CaptureCompanyNumberController.show().url
+            Ok(confirm_company(
+              companyName = companyName,
+              postAction = routes.ConfirmCompanyController.submit(),
+              changeLink = changeLink
+            ))
+          case _ =>
+            Redirect(routes.CaptureCompanyNumberController.show())
+        }
+      )
+    }
+  }
+
+  val submit: Action[AnyContent] = Action.async { implicit request =>
+    authorised() {
+      val optVatNumber = request.session.get(vatNumberKey).filter(_.nonEmpty)
+      val optCompanyNumber = request.session.get(companyNumberKey).filter(_.nonEmpty)
+
+      (optVatNumber, optCompanyNumber) match {
+        case (Some(vatNumber), Some(companyNumber)) =>
+          storeCompanyNumberService.storeCompanyNumber(vatNumber, companyNumber, companyUtr = None) map {
+            case Right(_) =>
+              Redirect(routes.EmailRoutingController.route().url)
+            case Left(errResponse) =>
+              throw new InternalServerException("storeCompanyNumber failed: status=" + errResponse.status)
+          }
+        case (None, _) =>
+          Future.successful(
+            Redirect(routes.CaptureVatNumberController.show())
+          )
+        case _ =>
+          Future.successful(
+            Redirect(routes.CaptureCompanyNumberController.show())
+          )
+      }
+
+    }
+  }
+
+}
---
+++ it/uk/gov/hmrc/vatsignupfrontend/controllers/agent/ConfirmCompanyControllerISpec.scala
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.controllers.agent
+
+import play.api.http.Status._
+import uk.gov.hmrc.vatsignupfrontend.SessionKeys
+import uk.gov.hmrc.vatsignupfrontend.config.featureswitch.CompanyNameJourney
+import uk.gov.hmrc.vatsignupfrontend.forms.EmailForm
+import uk.gov.hmrc.vatsignupfrontend.helpers.IntegrationTestConstants._
+import uk.gov.hmrc.vatsignupfrontend.helpers.servicemocks.AuthStub._
+import uk.gov.hmrc.vatsignupfrontend.helpers.servicemocks.StoreCompanyNumberStub._
+import uk.gov.hmrc.vatsignupfrontend.helpers.{ComponentSpecBase, CustomMatchers}
+
+class ConfirmCompanyControllerISpec extends ComponentSpecBase with CustomMatchers {
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    enable(CompanyNameJourney)
+  }
+
+  "GET /client/confirm-company" should {
+    "return an OK" in {
+      stubAuth(OK, successfulAuthResponse(agentEnrolment))
+
+      val res = get("/client/confirm-company", Map(SessionKeys.companyNameKey -> testCompanyName))
+
+      res should have(
+        httpStatus(OK)
+      )
+    }
+
+    "return a NOT_FOUND if CompanyNameJourney is disabled" in {
+      disable(CompanyNameJourney)
+
+      stubAuth(OK, successfulAuthResponse(agentEnrolment))
+
+      val res = get("/client/confirm-company", Map(SessionKeys.companyNameKey -> testCompanyName))
+
+      res should have(
+        httpStatus(NOT_FOUND)
+      )
+    }
+  }
+
+  "POST /client/confirm-company" should {
+    "return a NOT_FOUND if CompanyNameJourney is disabled" in {
+      disable(CompanyNameJourney)
+
+      stubAuth(OK, successfulAuthResponse(agentEnrolment))
+      stubStoreCompanyNumberSuccess(testVatNumber, testCompanyNumber)
+
+      val res = post("/client/confirm-company", Map(SessionKeys.companyNumberKey -> testCompanyNumber, SessionKeys.vatNumberKey -> testVatNumber))(EmailForm.email -> testEmail)
+
+      res should have(
+        httpStatus(NOT_FOUND)
+      )
+    }
+
+    "the company number is successfully stored" should {
+      "redirect to EmailRoutingController" in {
+        stubAuth(OK, successfulAuthResponse(agentEnrolment))
+        stubStoreCompanyNumberSuccess(testVatNumber, testCompanyNumber)
+
+        val res = post("/client/confirm-company", Map(SessionKeys.companyNumberKey -> testCompanyNumber, SessionKeys.vatNumberKey -> testVatNumber))(EmailForm.email -> testEmail)
+
+        res should have(
+          httpStatus(SEE_OTHER),
+          redirectUri(routes.EmailRoutingController.route().url)
+        )
+
+      }
+    }
+
+    "store company number returned error" should {
+      "INTERNAL_SERVER_ERROR" in {
+        stubAuth(OK, successfulAuthResponse(agentEnrolment))
+        stubStoreCompanyNumberFailure(testVatNumber, testCompanyNumber)
+
+        val res = post("/client/confirm-company", Map(SessionKeys.companyNumberKey -> testCompanyNumber, SessionKeys.vatNumberKey -> testVatNumber))(EmailForm.email -> testEmail)
+
+        res should have(
+          httpStatus(INTERNAL_SERVER_ERROR)
+        )
+      }
+    }
+
+  }
+
+}
