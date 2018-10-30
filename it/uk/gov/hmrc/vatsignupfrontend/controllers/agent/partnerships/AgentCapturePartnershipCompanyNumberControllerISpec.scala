fb8544561f4199357ae53cb931e43e2617140b68
Mohammed Ali Abidi <AliAbidi@Mohammeds-MacBook-Pro.local>
2018-10-30 13:14:47 +0000

    Controller for Capture Partnership CRN- Agent

---
+++ it/uk/gov/hmrc/vatsignupfrontend/controllers/agent/partnerships/AgentCapturePartnershipCompanyNumberControllerISpec.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.controllers.agent.partnerships
+
+import play.api.http.Status._
+import uk.gov.hmrc.vatsignupfrontend.config.featureswitch.LimitedPartnershipJourney
+import uk.gov.hmrc.vatsignupfrontend.forms.CompanyNumberForm
+import uk.gov.hmrc.vatsignupfrontend.helpers.IntegrationTestConstants._
+import uk.gov.hmrc.vatsignupfrontend.helpers.servicemocks.AuthStub._
+import uk.gov.hmrc.vatsignupfrontend.helpers.servicemocks.GetCompanyNameStub.stubgetCompanyName
+import uk.gov.hmrc.vatsignupfrontend.helpers.{ComponentSpecBase, CustomMatchers}
+import uk.gov.hmrc.vatsignupfrontend.models.companieshouse.LimitedPartnership
+
+class AgentCapturePartnershipCompanyNumberControllerISpec extends ComponentSpecBase with CustomMatchers {
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    enable(LimitedPartnershipJourney)
+  }
+
+  override def afterEach(): Unit = {
+    super.afterEach()
+    enable(LimitedPartnershipJourney)
+  }
+
+  "GET /partnership-company-number" should {
+    "return an OK" in {
+      stubAuth(OK, successfulAuthResponse())
+
+      val res = get("/partnership-company-number")
+
+      res should have(
+        httpStatus(OK)
+      )
+    }
+
+      "return an NOT FOUND" in {
+        disable(LimitedPartnershipJourney)
+        stubAuth(OK, successfulAuthResponse())
+
+        val res = get("/client/partnership-company-number")
+
+        res should have(
+          httpStatus(NOT_FOUND)
+        )
+      }
+  }
+
+  "POST /partnership-company-number" when {
+      "redirect to Confirm Partnership page" in {
+        stubAuth(OK, successfulAuthResponse())
+        stubgetCompanyName(testCompanyNumber, LimitedPartnership)
+
+        val res = post("/client/partnership-company-number")(CompanyNumberForm.companyNumber -> testCompanyNumber)
+
+        res should have(
+          httpStatus(SEE_OTHER),
+          redirectUri(routes.ConfirmPartnershipController.show().url)
+        )
+      }
+    }
+
+}
---
+++ test/uk/gov/hmrc/vatsignupfrontend/controllers/agent/partnerships/AgentCapturePartnershipNumberControllerSpec.scala
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.controllers.agent.partnerships
+
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.http.Status
+import play.api.mvc.AnyContentAsFormUrlEncoded
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.http.InternalServerException
+import uk.gov.hmrc.play.test.UnitSpec
+import uk.gov.hmrc.vatsignupfrontend.SessionKeys
+import uk.gov.hmrc.vatsignupfrontend.config.featureswitch.LimitedPartnershipJourney
+import uk.gov.hmrc.vatsignupfrontend.config.mocks.MockControllerComponents
+import uk.gov.hmrc.vatsignupfrontend.forms.CompanyNumberForm._
+import uk.gov.hmrc.vatsignupfrontend.helpers.TestConstants._
+import uk.gov.hmrc.vatsignupfrontend.models.companieshouse
+import uk.gov.hmrc.vatsignupfrontend.services.mocks.MockGetCompanyNameService
+
+class AgentCapturePartnershipNumberControllerSpec extends UnitSpec with GuiceOneAppPerSuite with MockControllerComponents
+  with MockGetCompanyNameService {
+
+    override def beforeEach(): Unit = {
+      super.beforeEach()
+      enable(LimitedPartnershipJourney)
+    }
+
+    override def afterEach(): Unit = {
+      super.afterEach()
+      enable(LimitedPartnershipJourney)
+    }
+
+    object TestCaptureCompanyNumberController extends AgentCapturePartnershipCompanyNumberController(
+      mockControllerComponents,
+      mockGetCompanyNameService
+    )
+
+    lazy val testGetRequest = FakeRequest("GET", "/partnership-company-number")
+
+    def testPostRequest(companyNumberVal: String): FakeRequest[AnyContentAsFormUrlEncoded] =
+      FakeRequest("POST", "/partnership-company-number").withFormUrlEncodedBody(companyNumber -> companyNumberVal)
+
+    "Calling the show action of the Capture Partnership Company Number controller" should {
+      "go to the Capture Partnership Company Number page" in {
+        mockAuthAdminRole()
+
+        val result = TestCaptureCompanyNumberController.show(testGetRequest)
+        status(result) shouldBe Status.OK
+        contentType(result) shouldBe Some("text/html")
+        charset(result) shouldBe Some("utf-8")
+      }
+    }
+
+    "Calling the submit action of the Capture Partnership Company Number controller" when {
+      "get company name returned successfully" should {
+        "Redirect to Confirm Partnership page" in {
+          mockAuthAdminRole()
+
+          mockGetCompanyNameSuccess(testCompanyNumber, companieshouse.LimitedPartnership)
+
+          val request = testPostRequest(testCompanyNumber)
+
+          val result = TestCaptureCompanyNumberController.submit(request)
+          status(result) shouldBe Status.SEE_OTHER
+          redirectLocation(result) shouldBe Some(routes.ConfirmPartnershipController.show().url)
+          session(result) get SessionKeys.partnershipTypeKey should contain(testPartnershipType)
+          session(result) get SessionKeys.companyNumberKey should contain(testCompanyNumber)
+          session(result) get SessionKeys.companyNameKey should contain(testCompanyName)
+        }
+      }
+
+      "form unsuccessfully submitted" should {
+        "reload the page with errors" in {
+          mockAuthAdminRole()
+
+          val result = TestCaptureCompanyNumberController.submit(testPostRequest("123456789"))
+          status(result) shouldBe Status.BAD_REQUEST
+          contentType(result) shouldBe Some("text/html")
+          charset(result) shouldBe Some("utf-8")
+        }
+      }
+
+      "company number failed prefix validation" should {
+        "throw an InternalServerException" in {
+          mockAuthAdminRole()
+          // TODO Redirect to error page
+
+          val testCrn = "ZZ12345"
+          val request = testPostRequest(testCrn)
+
+          val result = TestCaptureCompanyNumberController.submit(request)
+
+          intercept[InternalServerException](await(result))
+        }
+      }
+
+      "company number failed validation - invalid format" should {
+        "throw an InternalServerException" in {
+          mockAuthAdminRole()
+          // Redirect to error page
+
+          val testCrn = "123A456 A"
+          val request = testPostRequest(testCrn)
+
+          val result = TestCaptureCompanyNumberController.submit(request)
+
+          intercept[InternalServerException](await(result))
+        }
+      }
+
+      "company number failed validation - zero is invalid" should {
+        "throw an InternalServerException" in {
+          mockAuthAdminRole()
+          // Redirect to error page
+
+          val testCrn = "0"
+          val request = testPostRequest(testCrn)
+
+          val result = TestCaptureCompanyNumberController.submit(request)
+
+          intercept[InternalServerException](await(result))
+
+        }
+      }
+
+      "get company name returned not found" should {
+        "throw an InternalServerException" in {
+          mockAuthAdminRole()
+          // Redirect to error page
+
+          mockGetCompanyNameNotFound(testCompanyNumber)
+
+          val request = testPostRequest(testCompanyNumber)
+
+          val result = TestCaptureCompanyNumberController.submit(request)
+
+          intercept[InternalServerException](await(result))
+        }
+      }
+
+      "get company name fails" should {
+        "throw an InternalServerException" in {
+          mockAuthAdminRole()
+
+          mockGetCompanyNameFailure(testCompanyNumber)
+
+          val request = testPostRequest(testCompanyNumber)
+
+          val result = TestCaptureCompanyNumberController.submit(request)
+
+          intercept[InternalServerException](await(result))
+        }
+      }
+    }
+  }
