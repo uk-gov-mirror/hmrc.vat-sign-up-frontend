910ab14f6d3dbc7c4d550f6648e7a06da8c0f971
Jernej Vasic <jernej.vasic@digital.hmrc.gov.uk>
2018-11-22 11:20:11 +0000

    SAR-2909: Add connector and service for entity type unincorporated association

--- app/uk/gov/hmrc/vatsignupfrontend/config/AppConfig.scala
+++ app/uk/gov/hmrc/vatsignupfrontend/config/AppConfig.scala
@@ -92,6 +92,9 @@ class AppConfig @Inject()(val runModeConfiguration: Configuration, environment: 
   def storeAdministrativeDivisionUrl(vatNumber: String) =
     s"$protectedMicroServiceUrl/subscription-request/vat-number/$vatNumber/administrative-division"
 
+  def storeUnincorporatedAssociationInformationUrl(vatNumber: String): String =
+    s"$protectedMicroServiceUrl/subscription-request/vat-number/$vatNumber/unincorporated-association"
+
   def claimSubscriptionUrl(vatNumber: String) =
     s"$protectedMicroServiceUrl/claim-subscription/vat-number/$vatNumber"
 
