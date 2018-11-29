00510ca3fa6d718f3bbb051579804446b17574c3
Jernej Vasic <jernej.vasic@digital.hmrc.gov.uk>
2018-11-29 15:16:36 +0000

    SAR-3000: New resolver controller for unincorporated association
    New controller for the new entity type.
    NOTE: the service and connector have been already added in a previous
    user story.

---
+++ app/uk/gov/hmrc/vatsignupfrontend/controllers/principal/UnincorporatedAssociationResolverController.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.controllers.principal
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.http.InternalServerException
+import uk.gov.hmrc.vatsignupfrontend.SessionKeys
+import uk.gov.hmrc.vatsignupfrontend.config.ControllerComponents
+import uk.gov.hmrc.vatsignupfrontend.config.auth.AdministratorRolePredicate
+import uk.gov.hmrc.vatsignupfrontend.config.featureswitch.UnincorporatedAssociationJourney
+import uk.gov.hmrc.vatsignupfrontend.controllers.AuthenticatedController
+import uk.gov.hmrc.vatsignupfrontend.httpparsers.StoreUnincorporatedAssociationInformationHttpParser._
+import uk.gov.hmrc.vatsignupfrontend.services.StoreUnincorporatedAssociationInformationService
+
+import scala.concurrent.Future
+
+@Singleton
+class UnincorporatedAssociationResolverController @Inject()(val controllerComponents: ControllerComponents,
+                                                            storeUnincorporatedAssociationInformationService: StoreUnincorporatedAssociationInformationService
+                                                           )
+  extends AuthenticatedController(AdministratorRolePredicate, featureSwitches = Set(UnincorporatedAssociationJourney)) {
+
+  val resolve: Action[AnyContent] = Action.async { implicit request =>
+    authorised() {
+      val optVatNumber = request.session.get(SessionKeys.vatNumberKey).filter(_.nonEmpty)
+
+      optVatNumber match {
+        case Some(vatNumber) =>
+          storeUnincorporatedAssociationInformationService.storeUnincorporatedAssociationInformation(vatNumber = vatNumber) map {
+            case Right(StoreUnincorporatedAssociationInformationSuccess) =>
+              Redirect(routes.AgreeCaptureEmailController.show())
+            case Left(StoreUnincorporatedAssociationInformationFailureResponse(status)) =>
+              throw new InternalServerException("store unincorporated association information failed: status=" + status)
+          }
+        case _ =>
+          Future.successful(Redirect(routes.ResolveVatNumberController.resolve()))
+      }
+    }
+  }
+
+}
