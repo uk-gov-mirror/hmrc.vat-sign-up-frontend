dfcfa28102e158b1ebed05e3ad429ffe16e5d1c5
Simon Adams <simon.p.adams@hmrcaspire.com>
2018-12-07 11:44:43 +0000

    SAR-3032 - Added new view for Choose Software page

---
+++ test/uk/gov/hmrc/vatsignupfrontend/views/principal/ChooseSoftwareSpec.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.views.principal
+
+import play.api.i18n.Messages.Implicits._
+import play.api.i18n.MessagesApi
+import play.api.test.FakeRequest
+import play.api.{Configuration, Environment}
+import uk.gov.hmrc.vatsignupfrontend.assets.MessageLookup.{ChooseSoftware => messages}
+import uk.gov.hmrc.vatsignupfrontend.config.AppConfig
+import uk.gov.hmrc.vatsignupfrontend.views.ViewSpec
+
+class ChooseSoftwareSpec extends ViewSpec {
+
+  val env = Environment.simple()
+  val configuration = Configuration.load(env)
+
+  lazy val messagesApi = app.injector.instanceOf[MessagesApi]
+
+  lazy val page = uk.gov.hmrc.vatsignupfrontend.views.html.principal.choose_software(
+    postAction = testCall)(
+    FakeRequest(),
+    applicationMessages,
+    new AppConfig(configuration, env)
+  )
+
+  "The choose software view" should {
+
+    val testPage = TestView(
+      name = "choose software view",
+      title = messages.title,
+      heading = messages.heading,
+      page = page,
+      haveSignOutInBanner = false
+    )
+
+    testPage.shouldHaveParaSeq(
+      messages.line1,
+      messages.line2
+    )
+
+    testPage.shouldHaveForm("choose software form")(actionCall = testCall)
+
+    testPage.shouldHaveSubmitButton(messages.returnToGovUK)
+  }
+
+}
+
