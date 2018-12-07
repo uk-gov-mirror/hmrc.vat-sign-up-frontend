841a6fac4936063b0ceb414a3f0e602072cb7d17
robert-hawkes <robert.hawkes@digital.hmrc.gov.uk>
2018-12-07 13:01:50 +0000

    SAR-3063 - Create new controller for 'Choose software' error page

---
+++ app/uk/gov/hmrc/vatsignupfrontend/controllers/principal/ChooseSoftwareErrorController.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.controllers.principal
+
+import javax.inject.{Inject, Singleton}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import uk.gov.hmrc.vatsignupfrontend.config.{AppConfig, ControllerComponents}
+import uk.gov.hmrc.vatsignupfrontend.views.html.principal.choose_software_error
+
+@Singleton
+class ChooseSoftwareErrorController @Inject()(val controllerComponents: ControllerComponents) extends FrontendController with I18nSupport {
+
+  override val messagesApi: MessagesApi = controllerComponents.messagesApi
+
+  implicit val appConfig: AppConfig = controllerComponents.appConfig
+
+  val show: Action[AnyContent] = Action { implicit request =>
+    Ok(choose_software_error(routes.ChooseSoftwareErrorController.submit()))
+  }
+
+  val submit: Action[AnyContent] = Action { implicit request =>
+    Redirect(appConfig.guidancePageUrl)
+  }
+
+}
