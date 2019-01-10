6c908ee04ef69bf791e70a616f35189196aaee64
Mohammed Ali Abidi <AliAbidi@Mohammeds-MacBook-Pro.local>
2019-01-10 13:33:34 +0000

    WIP- Controller for Reg Soc Capture UTR

---
+++ test/uk/gov/hmrc/vatsignupfrontend/controllers/principal/CaptureRegisteredSocietyUtrControllerSpec.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.controllers.principal
+
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.http.Status
+import play.api.mvc.AnyContentAsFormUrlEncoded
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.play.test.UnitSpec
+import uk.gov.hmrc.vatsignupfrontend.SessionKeys
+import uk.gov.hmrc.vatsignupfrontend.config.mocks.MockControllerComponents
+import uk.gov.hmrc.vatsignupfrontend.forms.RegisteredSocietyUtrForm._
+import uk.gov.hmrc.vatsignupfrontend.helpers.TestConstants._
+import uk.gov.hmrc.vatsignupfrontend.config.featureswitch.RegisteredSocietyJourney
+
+class CaptureRegisteredSocietyUtrControllerSpec extends UnitSpec with GuiceOneAppPerSuite with MockControllerComponents {
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    enable(RegisteredSocietyJourney)
+  }
+
+  object TestCaptureRegisteredSocietyUtrController extends CaptureRegisteredSocietyUtrController(mockControllerComponents)
+
+  lazy val testGetRequest = FakeRequest("GET", "/registered-society-utr")
+
+  def testPostRequest(companyUtrVal: String): FakeRequest[AnyContentAsFormUrlEncoded] =
+    FakeRequest("POST", "/registered-society-utr").withFormUrlEncodedBody(registeredSocietyUtr -> companyUtrVal)
+
+  "Calling the show action of the Capture Registered Society Utr controller" should {
+    "go to the Capture Registered Society Utr page" in {
+      mockAuthAdminRole()
+
+      val result = TestCaptureRegisteredSocietyUtrController.show(testGetRequest)
+      status(result) shouldBe Status.OK
+      contentType(result) shouldBe Some("text/html")
+      charset(result) shouldBe Some("utf-8")
+    }
+  }
+
+  "Calling the submit action of the Capture Registered Society Utr controller" should {
+    "goto check your answers page" in {
+      mockAuthAdminRole()
+
+      val request = testPostRequest(testCompanyUtr)
+
+      val result = TestCaptureRegisteredSocietyUtrController.submit(request)
+
+      status(result) shouldBe Status.NOT_IMPLEMENTED
+     // redirectLocation(result) shouldBe Some(routes.RegisteredSocietyCheckYourAnswers.show().url)
+     //TODO Redirect to registered society check your answers page
+      //result.session(request).get(SessionKeys.companyUtrKey) shouldBe Some(testCompanyUtr)
+    }
+  }
+
+  "form unsuccessfully submitted" should {
+    "reload the page with errors" in {
+      mockAuthAdminRole()
+
+      val result = TestCaptureRegisteredSocietyUtrController.submit(testPostRequest("invalid"))
+      status(result) shouldBe Status.BAD_REQUEST
+      contentType(result) shouldBe Some("text/html")
+      charset(result) shouldBe Some("utf-8")
+    }
+  }
+
+}
