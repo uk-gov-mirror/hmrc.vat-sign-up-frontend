a20c256d4303a7300b4318866442b33027ad28cc
r-melvin <40235254+r-melvin@users.noreply.github.com>
2019-02-22 12:23:04 +0000

    SAR-3335: controller for previously submitted vat return page

---
+++ app/uk/gov/hmrc/vatsignupfrontend/controllers/principal/PreviousVatReturnController.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.controllers.principal
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.vatsignupfrontend.SessionKeys
+import uk.gov.hmrc.vatsignupfrontend.config.ControllerComponents
+import uk.gov.hmrc.vatsignupfrontend.config.auth.AdministratorRolePredicate
+import uk.gov.hmrc.vatsignupfrontend.config.featureswitch.{AdditionalKnownFacts, FeatureSwitch, FeatureSwitching}
+import uk.gov.hmrc.vatsignupfrontend.controllers.AuthenticatedController
+import uk.gov.hmrc.vatsignupfrontend.forms.PreviousVatReturnForm._
+import uk.gov.hmrc.vatsignupfrontend.models.{No, Yes}
+import uk.gov.hmrc.vatsignupfrontend.views.html.principal.previous_vat_return
+
+import scala.concurrent.Future
+
+@Singleton
+class PreviousVatReturnController @Inject()(val controllerComponents: ControllerComponents)
+
+  extends AuthenticatedController(AdministratorRolePredicate, featureSwitches = Set(AdditionalKnownFacts)){
+
+  val show: Action[AnyContent] = Action.async {
+    implicit request =>
+      authorised() {
+        Future.successful(
+          Ok(previous_vat_return(previousVatReturnForm, routes.PreviousVatReturnController.submit()))
+        )
+      }
+  }
+
+  val submit: Action[AnyContent] = Action.async {
+    implicit request =>
+      authorised() {
+        previousVatReturnForm.bindFromRequest.fold(
+          formWithErrors =>
+            Future.successful(
+              BadRequest(previous_vat_return(formWithErrors, routes.PreviousVatReturnController.submit()))
+            ), {
+            case Yes =>
+              Future.successful(
+                NotImplemented //TODO: Redirect to Box 5 page
+                  .addingToSession(SessionKeys.previousVatReturnKey -> Yes.toString)
+              )
+            case No =>
+              Future.successful(
+                Redirect(routes.CheckYourAnswersController.show())
+                  .addingToSession(SessionKeys.previousVatReturnKey -> No.toString)
+              )
+          }
+        )
+      }
+  }
+
+}
---
+++ test/uk/gov/hmrc/vatsignupfrontend/controllers/principal/PreviousVatReturnControllerSpec.scala
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.controllers.principal
+
+import play.api.http.Status
+import play.api.mvc.AnyContentAsFormUrlEncoded
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.play.test.UnitSpec
+import uk.gov.hmrc.vatsignupfrontend.SessionKeys
+import uk.gov.hmrc.vatsignupfrontend.config.featureswitch.AdditionalKnownFacts
+import uk.gov.hmrc.vatsignupfrontend.config.mocks.MockControllerComponents
+import uk.gov.hmrc.vatsignupfrontend.forms.PreviousVatReturnForm._
+import uk.gov.hmrc.vatsignupfrontend.models.{No, Yes}
+
+class PreviousVatReturnControllerSpec extends UnitSpec with MockControllerComponents {
+
+  object TestPreviousVatReturnController extends PreviousVatReturnController(mockControllerComponents)
+
+  val testGetRequest = FakeRequest("GET", "/submitted-vat-return")
+
+  def testPostRequest(usersChoice: String): FakeRequest[AnyContentAsFormUrlEncoded] =
+    FakeRequest("POST", "/submitted-vat-return").withFormUrlEncodedBody(yesNo -> usersChoice)
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    enable(AdditionalKnownFacts)
+  }
+
+  override def afterEach(): Unit = {
+    super.afterEach()
+    disable(AdditionalKnownFacts)
+  }
+
+  "Calling the show action of the Previous Vat Return controller" when {
+      "go to the Previous Vat Return page" in {
+        mockAuthAdminRole()
+
+        val result = TestPreviousVatReturnController.show(testGetRequest)
+        status(result) shouldBe Status.OK
+        contentType(result) shouldBe Some("text/html")
+        charset(result) shouldBe Some("utf-8")
+      }
+    }
+
+  "Calling the submit action of the Previous Vat Return controller" when {
+    "form successfully submitted" should {
+      "the choice is YES" should {
+        "go to vat number" in {
+          mockAuthAdminRole()
+
+          val result = TestPreviousVatReturnController.submit(testPostRequest(usersChoice = "yes"))
+          status(result) shouldBe Status.NOT_IMPLEMENTED
+          result.session(testPostRequest(usersChoice = "no")).get(SessionKeys.previousVatReturnKey) shouldBe Some(Yes.toString)
+        }
+      }
+
+      "the choice is NO" when {
+        "the VAT number is stored successfully" should {
+          "go to check your answers" in {
+            mockAuthAdminRole()
+
+            val result = TestPreviousVatReturnController.submit(testPostRequest(usersChoice = "no"))
+            status(result) shouldBe Status.SEE_OTHER
+            redirectLocation(result) shouldBe Some(routes.CheckYourAnswersController.show().url)
+            result.session(testPostRequest(usersChoice = "no")).get(SessionKeys.previousVatReturnKey) shouldBe Some(No.toString)
+          }
+        }
+      }
+
+      "form unsuccessfully submitted" should {
+        "reload the page with errors" in {
+          mockAuthAdminRole()
+
+          val result = TestPreviousVatReturnController.submit(testPostRequest(usersChoice = "invalid"))
+          status(result) shouldBe Status.BAD_REQUEST
+          contentType(result) shouldBe Some("text/html")
+          charset(result) shouldBe Some("utf-8")
+        }
+      }
+    }
+  }
+}
