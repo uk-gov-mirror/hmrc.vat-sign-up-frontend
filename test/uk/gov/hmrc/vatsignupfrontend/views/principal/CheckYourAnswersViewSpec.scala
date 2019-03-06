MJCallahanPage/vat-sign-up-frontend

Adding missing question to CYA page

test/uk/gov/hmrc/vatsignupfrontend/views/principal/CheckYourAnswersViewSpec.scala
@@ -27,7 +27,7 @@ import play.twirl.api.Html
 import uk.gov.hmrc.vatsignupfrontend.assets.MessageLookup.{PrincipalCheckYourAnswers => messages}
 import uk.gov.hmrc.vatsignupfrontend.config.AppConfig
 import uk.gov.hmrc.vatsignupfrontend.helpers.TestConstants._
-import uk.gov.hmrc.vatsignupfrontend.models.{DateModel, PostCode, SoleTrader}
+import uk.gov.hmrc.vatsignupfrontend.models.{DateModel, PostCode, SoleTrader, Yes}
 import uk.gov.hmrc.vatsignupfrontend.views.ViewSpec
 import uk.gov.hmrc.vatsignupfrontend.views.helpers.CheckYourAnswersIdConstants._
 
@@ -40,23 +40,25 @@ class CheckYourAnswersViewSpec extends ViewSpec {
   val env = Environment.simple()
   val configuration = Configuration.load(env)
 
-  def page(optBox5Value: Option[String] = None,
+  def page(optBox5Figure: Option[String] = None,
            optLastReturnMonthPeriod: Option[String] = None,
+           optPreviousVatReturn: Option[String] = None,
            optPostCode: Option[PostCode] = Some(testBusinessPostcode)
           ): Html = uk.gov.hmrc.vatsignupfrontend.views.html.principal.check_your_answers(
     vatNumber = testVatNumber,
     registrationDate = testRegistrationDate,
     optPostCode = optPostCode,
-    optBox5Value = optBox5Value,
+    optPreviousVatReturn = optPreviousVatReturn,
+    optBox5Figure = optBox5Figure,
     optLastReturnMonthPeriod = optLastReturnMonthPeriod,
     postAction = testCall
   )(FakeRequest(), applicationMessages, new AppConfig(configuration, env))
 
   lazy val pageDefault = page()
 
-  lazy val pageWithAdditionalKnownFacts = page(Some(testBox5Figure), Some(testLastReturnMonthPeriod))
+  lazy val pageWithAdditionalKnownFacts = page(Some(testBox5Figure), Some(testLastReturnMonthPeriod), Some(Yes.toString))
 
-  lazy val pageWithoutOverseasPostCode = page(Some(testBox5Figure), Some(testLastReturnMonthPeriod), None)
+  lazy val pageWithoutOverseasPostCode = page(Some(testBox5Figure), Some(testLastReturnMonthPeriod), Some(Yes.toString), None)
 
   val questionId: String => String = (sectionId: String) => s"$sectionId-question"
   val answerId: String => String = (sectionId: String) => s"$sectionId-answer"
@@ -193,13 +195,25 @@ class CheckYourAnswersViewSpec extends ViewSpec {
       )
     }
 
+    "display the correct answer for Previous VAT return" in {
+      val expectedEditLink = uk.gov.hmrc.vatsignupfrontend.controllers.principal.routes.PreviousVatReturnController.show().url
+
+      sectionTest(
+        page = pageWithAdditionalKnownFacts,
+        sectionId = PreviousVatReturnId,
+        expectedQuestion = messages.previousVatReturn,
+        expectedAnswer = Yes.toString,
+        expectedEditLink = Some(expectedEditLink)
+      )
+    }
+
     "display the correct answer for Box5Value" in {
       val expectedEditLink = uk.gov.hmrc.vatsignupfrontend.controllers.principal.routes.CaptureBox5FigureController.show().url
 
       sectionTest(
         page = pageWithAdditionalKnownFacts,
-        sectionId = VatBox5ValueId,
-        expectedQuestion = messages.box5Value,
+        sectionId = VatBox5FigureId,
+        expectedQuestion = messages.box5Figure,
         expectedAnswer = s"£99,999,999,999.99",
         expectedEditLink = Some(expectedEditLink)
       )
@@ -260,9 +274,21 @@ class CheckYourAnswersViewSpec extends ViewSpec {
 
       sectionTest(
         page = pageWithAdditionalKnownFacts,
-        sectionId = VatBox5ValueId,
-        expectedQuestion = messages.box5Value,
+        sectionId = VatBox5FigureId,
+        expectedQuestion = messages.box5Figure,
         expectedAnswer = s"£99,999,999,999.99",
+        expectedEditLink = Some(expectedEditLink)
+      )
+    }
+
+    "display the correct answer for Previous VAT return" in {
+      val expectedEditLink = uk.gov.hmrc.vatsignupfrontend.controllers.principal.routes.PreviousVatReturnController.show().url
+
+      sectionTest(
+        page = pageWithAdditionalKnownFacts,
+        sectionId = PreviousVatReturnId,
+        expectedQuestion = messages.previousVatReturn,
+        expectedAnswer = Yes.toString,
         expectedEditLink = Some(expectedEditLink)
       )
     }

uk-gov-mirror/hmrc.vat-sign-up-frontend

test/uk/gov/hmrc/vatsignupfrontend/views/principal/CheckYourAnswersViewSpec.scala
@@ -27,7 +27,7 @@ import play.twirl.api.Html
 import uk.gov.hmrc.vatsignupfrontend.assets.MessageLookup.{PrincipalCheckYourAnswers => messages}
 import uk.gov.hmrc.vatsignupfrontend.config.AppConfig
 import uk.gov.hmrc.vatsignupfrontend.helpers.TestConstants._
-import uk.gov.hmrc.vatsignupfrontend.models.{DateModel, PostCode, SoleTrader}
+import uk.gov.hmrc.vatsignupfrontend.models.{DateModel, PostCode, SoleTrader, Yes}
 import uk.gov.hmrc.vatsignupfrontend.views.ViewSpec
 import uk.gov.hmrc.vatsignupfrontend.views.helpers.CheckYourAnswersIdConstants._
 
@@ -40,23 +40,25 @@ class CheckYourAnswersViewSpec extends ViewSpec {
   val env = Environment.simple()
   val configuration = Configuration.load(env)
 
-  def page(optBox5Value: Option[String] = None,
+  def page(optBox5Figure: Option[String] = None,
            optLastReturnMonthPeriod: Option[String] = None,
+           optPreviousVatReturn: Option[String] = None,
            optPostCode: Option[PostCode] = Some(testBusinessPostcode)
           ): Html = uk.gov.hmrc.vatsignupfrontend.views.html.principal.check_your_answers(
     vatNumber = testVatNumber,
     registrationDate = testRegistrationDate,
     optPostCode = optPostCode,
-    optBox5Value = optBox5Value,
+    optPreviousVatReturn = optPreviousVatReturn,
+    optBox5Figure = optBox5Figure,
     optLastReturnMonthPeriod = optLastReturnMonthPeriod,
     postAction = testCall
   )(FakeRequest(), applicationMessages, new AppConfig(configuration, env))
 
   lazy val pageDefault = page()
 
-  lazy val pageWithAdditionalKnownFacts = page(Some(testBox5Figure), Some(testLastReturnMonthPeriod))
+  lazy val pageWithAdditionalKnownFacts = page(Some(testBox5Figure), Some(testLastReturnMonthPeriod), Some(Yes.stringValue))
 
-  lazy val pageWithoutOverseasPostCode = page(Some(testBox5Figure), Some(testLastReturnMonthPeriod), None)
+  lazy val pageWithoutPostCode = page(Some(testBox5Figure), Some(testLastReturnMonthPeriod), Some(Yes.stringValue), None)
 
   val questionId: String => String = (sectionId: String) => s"$sectionId-question"
   val answerId: String => String = (sectionId: String) => s"$sectionId-answer"
@@ -193,13 +195,25 @@ class CheckYourAnswersViewSpec extends ViewSpec {
       )
     }
 
+    "display the correct answer for Previous VAT return" in {
+      val expectedEditLink = uk.gov.hmrc.vatsignupfrontend.controllers.principal.routes.PreviousVatReturnController.show().url
+
+      sectionTest(
+        page = pageWithAdditionalKnownFacts,
+        sectionId = PreviousVatReturnId,
+        expectedQuestion = messages.previousVatReturn,
+        expectedAnswer = Yes.stringValue,
+        expectedEditLink = Some(expectedEditLink)
+      )
+    }
+
     "display the correct answer for Box5Value" in {
       val expectedEditLink = uk.gov.hmrc.vatsignupfrontend.controllers.principal.routes.CaptureBox5FigureController.show().url
 
       sectionTest(
         page = pageWithAdditionalKnownFacts,
-        sectionId = VatBox5ValueId,
-        expectedQuestion = messages.box5Value,
+        sectionId = VatBox5FigureId,
+        expectedQuestion = messages.box5Figure,
         expectedAnswer = s"£99,999,999,999.99",
         expectedEditLink = Some(expectedEditLink)
       )
@@ -224,7 +238,7 @@ class CheckYourAnswersViewSpec extends ViewSpec {
       name = "Check your answers View",
       title = messages.title,
       heading = messages.heading,
-      page = pageWithoutOverseasPostCode
+      page = pageWithoutPostCode
     )
 
     testPage.shouldHaveH2(messages.subHeading)
@@ -260,9 +274,21 @@ class CheckYourAnswersViewSpec extends ViewSpec {
 
       sectionTest(
         page = pageWithAdditionalKnownFacts,
-        sectionId = VatBox5ValueId,
-        expectedQuestion = messages.box5Value,
+        sectionId = VatBox5FigureId,
+        expectedQuestion = messages.box5Figure,
         expectedAnswer = s"£99,999,999,999.99",
+        expectedEditLink = Some(expectedEditLink)
+      )
+    }
+
+    "display the correct answer for Previous VAT return" in {
+      val expectedEditLink = uk.gov.hmrc.vatsignupfrontend.controllers.principal.routes.PreviousVatReturnController.show().url
+
+      sectionTest(
+        page = pageWithAdditionalKnownFacts,
+        sectionId = PreviousVatReturnId,
+        expectedQuestion = messages.previousVatReturn,
+        expectedAnswer = Yes.stringValue,
         expectedEditLink = Some(expectedEditLink)
       )
     }