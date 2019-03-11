aaa926a43d5e2fdb39471cab1cbc43fbaa296f8b
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2019-03-11 13:39:44 +0000

    [SAR-3436] Added controller and unit tests for Agent ContactPrefsController

---
+++ app/uk/gov/hmrc/vatsignupfrontend/controllers/agent/ContactPreferenceController.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.controllers.agent
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{Action, AnyContent, Result}
+import uk.gov.hmrc.http.InternalServerException
+import uk.gov.hmrc.vatsignupfrontend.SessionKeys
+import uk.gov.hmrc.vatsignupfrontend.config.ControllerComponents
+import uk.gov.hmrc.vatsignupfrontend.config.auth.AgentEnrolmentPredicate
+import uk.gov.hmrc.vatsignupfrontend.config.featureswitch.ContactPrefencesJourney
+import uk.gov.hmrc.vatsignupfrontend.controllers.AuthenticatedController
+import uk.gov.hmrc.vatsignupfrontend.forms.ContactPreferencesForm._
+import uk.gov.hmrc.vatsignupfrontend.models.{ContactPreference, Digital}
+import uk.gov.hmrc.vatsignupfrontend.services.StoreContactPreferenceService
+import uk.gov.hmrc.vatsignupfrontend.views.html.agent.receive_email_notifications
+
+import scala.concurrent.Future
+
+@Singleton
+class ContactPreferenceController @Inject()(val controllerComponents: ControllerComponents,
+                                            val contactPreferenceService: StoreContactPreferenceService)
+  extends AuthenticatedController(AgentEnrolmentPredicate, Set(ContactPrefencesJourney)) {
+
+  val show: Action[AnyContent] = Action.async { implicit request =>
+    authorised() {
+      Future.successful(
+        Ok(receive_email_notifications(contactPreferencesForm(isAgent = true), routes.ConfirmClientEmailController.submit()))
+      )
+    }
+  }
+
+  val submit: Action[AnyContent] = Action.async { implicit request =>
+    authorised() {
+
+      val optVatNumber = request.session.get(SessionKeys.vatNumberKey).filter(_.nonEmpty)
+      val hasDirectDebit = request.session.get(SessionKeys.hasDirectDebitKey).getOrElse("false").toBoolean
+
+      def storeContactPreference(contactPreference: ContactPreference): Future[Result] = {
+        optVatNumber match {
+          case Some(vatNumber) =>
+            contactPreferenceService.storeContactPreference(vatNumber, contactPreference) map {
+              case Right(_) => redirect(contactPreference)
+              case Left(status) => throw new InternalServerException(s"Store contact preference failed with status = $status")
+            }
+          case None =>
+            Future.successful(Redirect(routes.CaptureVatNumberController.show()))
+        }
+      }
+
+      def redirect(contactPreference: ContactPreference): Result =
+        if(contactPreference == Digital || hasDirectDebit) {
+          Redirect(routes.CaptureClientEmailController.show())
+        } else {
+          Redirect(routes.TermsController.show())
+        }
+
+      contactPreferencesForm(isAgent = true).bindFromRequest.fold(
+        formWithErrors => Future.successful(
+          BadRequest(receive_email_notifications(formWithErrors, routes.ConfirmClientEmailController.submit()))
+        ),
+        storeContactPreference
+      )
+    }
+  }
+}
---
+++ test/uk/gov/hmrc/vatsignupfrontend/controllers/agent/ContactPreferenceControllerSpec.scala
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.controllers.agent
+
+import play.api.http.Status
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.http.InternalServerException
+import uk.gov.hmrc.vatsignupfrontend.SessionKeys
+import uk.gov.hmrc.vatsignupfrontend.assets.MessageLookup
+import uk.gov.hmrc.vatsignupfrontend.config.featureswitch.ContactPrefencesJourney
+import uk.gov.hmrc.vatsignupfrontend.controllers.ControllerSpec
+import uk.gov.hmrc.vatsignupfrontend.forms.ContactPreferencesForm
+import uk.gov.hmrc.vatsignupfrontend.helpers.TestConstants._
+import uk.gov.hmrc.vatsignupfrontend.models.{Digital, Paper}
+import uk.gov.hmrc.vatsignupfrontend.services.mocks.MockStoreContactPreferenceService
+
+class ContactPreferenceControllerSpec extends ControllerSpec with MockStoreContactPreferenceService {
+
+  object TestContactPreferenceController extends ContactPreferenceController(mockControllerComponents, mockStoreContactPreferenceService)
+
+  lazy val testGetRequest = FakeRequest("GET", "/receive-email-notifications")
+  lazy val testPostRequest = FakeRequest("POST", "/receive-email-notifications")
+
+  "Calling the show action of the Contact Preference controller" should {
+
+    lazy val result = TestContactPreferenceController.show(testGetRequest)
+
+    "return status OK (200)" in {
+      enable(ContactPrefencesJourney)
+      mockAuthRetrieveAgentEnrolment()
+      status(result) shouldBe Status.OK
+    }
+
+    "have the html as the content type" in {
+      contentType(result) shouldBe Some("text/html")
+    }
+
+    "return the charset utf-8" in {
+      charset(result) shouldBe Some("utf-8")
+    }
+
+    "render the receive email notifications view" in {
+      titleOf(result) shouldBe MessageLookup.AgentReceiveEmailNotifications.title
+    }
+  }
+
+  "Calling the submit action of the Contact Preference controller" when {
+
+    "vat number is in session" when {
+
+      "call to store contact preference is successful" when {
+
+        "contact preference is Digital" should {
+
+          "redirect to Capture Client Email page" in {
+            enable(ContactPrefencesJourney)
+            mockAuthRetrieveAgentEnrolment()
+            mockStoreContactPreferenceSuccess(vatNumber = testVatNumber, contactPreference = Digital)
+
+            val request = testPostRequest.withSession(SessionKeys.vatNumberKey -> testVatNumber)
+              .withFormUrlEncodedBody(ContactPreferencesForm.contactPreference -> ContactPreferencesForm.digital)
+
+            val result = TestContactPreferenceController.submit()(request)
+
+            status(result) shouldBe Status.SEE_OTHER
+            redirectLocation(result) shouldBe Some(routes.CaptureClientEmailController.show().url)
+          }
+        }
+
+        "contact preference is Paper" when {
+
+          "user has direct debit" should {
+
+            "redirect to Capture Client Email page" in {
+              enable(ContactPrefencesJourney)
+              mockAuthRetrieveAgentEnrolment()
+              mockStoreContactPreferenceSuccess(vatNumber = testVatNumber, contactPreference = Paper)
+
+              val request = testPostRequest.withSession(SessionKeys.vatNumberKey -> testVatNumber, SessionKeys.hasDirectDebitKey -> "true")
+                .withFormUrlEncodedBody(ContactPreferencesForm.contactPreference -> ContactPreferencesForm.paper)
+
+              val result = TestContactPreferenceController.submit()(request)
+
+              status(result) shouldBe Status.SEE_OTHER
+              redirectLocation(result) shouldBe Some(routes.CaptureClientEmailController.show().url)
+            }
+          }
+
+          "user does not have a direct debit" should {
+
+            "redirect to Terms page" in {
+              enable(ContactPrefencesJourney)
+              mockAuthRetrieveAgentEnrolment()
+              mockStoreContactPreferenceSuccess(vatNumber = testVatNumber, contactPreference = Paper)
+
+              val request = testPostRequest.withSession(SessionKeys.vatNumberKey -> testVatNumber)
+                .withFormUrlEncodedBody(ContactPreferencesForm.contactPreference -> ContactPreferencesForm.paper)
+
+              val result = TestContactPreferenceController.submit()(request)
+
+              status(result) shouldBe Status.SEE_OTHER
+              redirectLocation(result) shouldBe Some(routes.TermsController.show().url)
+            }
+          }
+
+        }
+      }
+
+      "call to store contact preference is NOT successful" when {
+
+        "redirect to Capture Client Email page" in {
+          enable(ContactPrefencesJourney)
+          mockAuthRetrieveAgentEnrolment()
+          mockStoreContactPreferenceFailure(vatNumber = testVatNumber, contactPreference = Digital)
+
+          val request = testPostRequest.withSession(SessionKeys.vatNumberKey -> testVatNumber)
+            .withFormUrlEncodedBody(ContactPreferencesForm.contactPreference -> ContactPreferencesForm.digital)
+
+          val result = TestContactPreferenceController.submit()(request)
+
+          intercept[InternalServerException](await(TestContactPreferenceController.submit(request)))
+        }
+      }
+    }
+
+    "vat number is NOT in session" should {
+
+      "redirect to Capture Client VRN page" in {
+        enable(ContactPrefencesJourney)
+        mockAuthRetrieveAgentEnrolment()
+
+        val request = testPostRequest.withFormUrlEncodedBody(ContactPreferencesForm.contactPreference -> ContactPreferencesForm.digital)
+
+        val result = TestContactPreferenceController.submit()(request)
+
+        status(result) shouldBe Status.SEE_OTHER
+        redirectLocation(result) shouldBe Some(routes.CaptureVatNumberController.show().url)
+      }
+    }
+  }
+}
