4acad37c26ae66e9ed8188650952280bb4a70b5c
Ali Abidi <28566475+mohammed-abidi@users.noreply.github.com>
2019-04-04 10:29:36 +0100

    Redirect for Failed Client Matching Controller

--- test/uk/gov/hmrc/vatsignupfrontend/controllers/agent/FailedClientMatchingControllerSpec.scala
+++ test/uk/gov/hmrc/vatsignupfrontend/controllers/agent/FailedClientMatchingControllerSpec.scala
@@ -22,12 +22,14 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.play.test.UnitSpec
 import uk.gov.hmrc.vatsignupfrontend.config.mocks.MockControllerComponents
+import uk.gov.hmrc.vatsignupfrontend.views.html.agent.failed_client_matching
+import play.api.i18n.Messages.Implicits._
 
 class FailedClientMatchingControllerSpec extends UnitSpec with GuiceOneAppPerSuite with MockControllerComponents {
 
   object TestFailedClientMatchingController extends FailedClientMatchingController(mockControllerComponents)
 
-  lazy val testGetRequest = FakeRequest("GET", "/error/incorrect-details")
+  implicit lazy val testGetRequest = FakeRequest("GET", "/error/incorrect-details")
 
   "Calling the show action of the Failed Client Matching controller" should {
     "return OK" in {
@@ -39,5 +41,13 @@ class FailedClientMatchingControllerSpec extends UnitSpec with GuiceOneAppPerSui
       contentType(result) shouldBe Some("text/html")
       charset(result) shouldBe Some("utf-8")
     }
+  }
+
+  "go to the Sign up Another Client page" in {
+    mockAuthRetrieveAgentEnrolment()
+    val result = await(TestFailedClientMatchingController.show(testGetRequest))
+
+    status(result) shouldBe Status.OK
+    contentAsString(result) shouldBe failed_client_matching(routes.SignUpAnotherClientController.submit()).body
   }
 }
