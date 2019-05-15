c04d604381273c484131757b95e39798dc8cec70
dwalters <david-walters-HMRC@.noreply.github.com>
2019-05-15 16:09:44 +0100

    SAR-3625 - Skip entering email, if agent email in session. Updated the change email link to hit different route to which removes the agent email from session before directing to the main page

--- it/uk/gov/hmrc/vatsignupfrontend/controllers/agent/CaptureAgentEmailControllerISpec.scala
+++ it/uk/gov/hmrc/vatsignupfrontend/controllers/agent/CaptureAgentEmailControllerISpec.scala
@@ -25,6 +25,8 @@ import uk.gov.hmrc.vatsignupfrontend.helpers.{ComponentSpecBase, CustomMatchers,
 
 class CaptureAgentEmailControllerISpec extends ComponentSpecBase with CustomMatchers {
 
+
+
   "GET /email-address" should {
     "return an OK" in {
       stubAuth(OK, successfulAuthResponse(agentEnrolment))
@@ -33,6 +35,18 @@ class CaptureAgentEmailControllerISpec extends ComponentSpecBase with CustomMatc
 
       res should have(
         httpStatus(OK)
+      )
+    }
+  }
+
+  "GET /email-address-change" should {
+    "return an OK" in {
+      stubAuth(OK, successfulAuthResponse(agentEnrolment))
+
+      val res = get("/client/email-address-change")
+
+      res should have(
+        httpStatus(SEE_OTHER)
       )
     }
   }
