08fffa5dc11da08a0272810a496ecd2c8ec10593
Valentin Lukovski <47025947+VLukovski@users.noreply.github.com>
2019-10-28 10:22:40 +0000

    SAR-4743 refactored the orchestration service to handle enrolments, removed enrolment logic from multiple vat number controller

--- app/uk/gov/hmrc/vatsignupfrontend/controllers/principal/MultipleVatCheckController.scala
+++ app/uk/gov/hmrc/vatsignupfrontend/controllers/principal/MultipleVatCheckController.scala
@@ -17,13 +17,12 @@
 package uk.gov.hmrc.vatsignupfrontend.controllers.principal
 
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.{Action, AnyContent, RequestHeader, Result}
+import play.api.mvc.{Action, AnyContent}
 import uk.gov.hmrc.auth.core.retrieve.Retrievals
-import uk.gov.hmrc.http.{HeaderCarrier, InternalServerException}
+import uk.gov.hmrc.http.InternalServerException
 import uk.gov.hmrc.vatsignupfrontend.SessionKeys
 import uk.gov.hmrc.vatsignupfrontend.config.ControllerComponents
 import uk.gov.hmrc.vatsignupfrontend.config.auth.AdministratorRolePredicate
-import uk.gov.hmrc.vatsignupfrontend.config.featureswitch.ReSignUpJourney
 import uk.gov.hmrc.vatsignupfrontend.controllers.AuthenticatedController
 import uk.gov.hmrc.vatsignupfrontend.forms.MultipleVatCheckForm._
 import uk.gov.hmrc.vatsignupfrontend.models.{No, Yes}
@@ -48,80 +47,49 @@ class MultipleVatCheckController @Inject()(val controllerComponents: ControllerC
 
   val submit: Action[AnyContent] = Action.async { implicit request =>
     authorised()(Retrievals.allEnrolments) { enrolments =>
-      if (isEnabled(ReSignUpJourney))
-        enrolments.getAnyVatNumber match {
-          case Some(vatNumber) =>
-            multipleVatCheckForm.bindFromRequest.fold(
-              formWithErrors =>
-                Future.successful(
-                  BadRequest(multiple_vat_check(formWithErrors, routes.MultipleVatCheckController.submit()))
-                ), {
-                case Yes =>
-                  Future.successful(Redirect(routes.CaptureVatNumberController.show()))
-                case No =>
-                  storeVat(vatNumber, isFromBta = false)
-              }
-            )
-          case _ =>
-            Future.successful(
-              Redirect(routes.ResolveVatNumberController.resolve())
-            )
+
+      multipleVatCheckForm.bindFromRequest.fold(
+        formWithErrors =>
+          Future.successful(
+            BadRequest(multiple_vat_check(formWithErrors, routes.MultipleVatCheckController.submit()))
+          ), {
+          case Yes =>
+            Future.successful(Redirect(routes.CaptureVatNumberController.show()))
+          case No =>
+            enrolments.getAnyVatNumber match {
+              case None =>
+                Future.successful(Redirect(routes.ResolveVatNumberController.resolve()))
+              case Some(_) =>
+                vatNumberOrchestrationService.orchestrate(enrolments, None, isFromBta = false) map {
+                  case VatNumberStored(isOverseas, isDirectDebit, isMigrated) if isOverseas =>
+                    Redirect(routes.OverseasResolverController.resolve())
+                      .addingToSession(SessionKeys.vatNumberKey -> enrolments.getAnyVatNumber.get)
+                      .addingToSession(SessionKeys.isMigratedKey, isMigrated)
+                      .addingToSession(SessionKeys.hasDirectDebitKey, isDirectDebit)
+                  case VatNumberStored(_, isDirectDebit, isMigrated) =>
+                    Redirect(routes.CaptureBusinessEntityController.show())
+                      .addingToSession(SessionKeys.vatNumberKey -> enrolments.getAnyVatNumber.get)
+                      .addingToSession(SessionKeys.isMigratedKey, isMigrated)
+                      .addingToSession(SessionKeys.hasDirectDebitKey, isDirectDebit)
+                  case ClaimedSubscription =>
+                    Redirect(routes.SignUpCompleteClientController.show())
+                  case Ineligible =>
+                    Redirect(routes.CannotUseServiceController.show())
+                  case Inhibited(migratableDates) =>
+                    Redirect(routes.MigratableDatesController.show())
+                      .addingToSession(SessionKeys.migratableDatesKey, migratableDates)
+                  case MigrationInProgress =>
+                    Redirect(routes.MigrationInProgressErrorController.show())
+                  case AlreadyEnrolledOnDifferentCredential =>
+                    Redirect(bta.routes.BusinessAlreadySignedUpController.show())
+                  case AlreadySubscribed =>
+                    Redirect(routes.AlreadySignedUpController.show())
+                  case _ =>
+                    throw new InternalServerException("Unexpected response from vat number orchestration service")
+                }
+            }
         }
-      else
-        enrolments.vatNumber match {
-          case Some(vatNumber) =>
-            multipleVatCheckForm.bindFromRequest.fold(
-              formWithErrors =>
-                Future.successful(
-                  BadRequest(multiple_vat_check(formWithErrors, routes.MultipleVatCheckController.submit()))
-                ), {
-                case Yes =>
-                  Future.successful(Redirect(routes.CaptureVatNumberController.show()))
-                case No =>
-                  enrolments.mtdVatNumber match {
-                    case Some(mtdVatNumber) if mtdVatNumber == vatNumber =>
-                      Future.successful(Redirect(routes.AlreadySignedUpController.show()))
-                    case Some(_) =>
-                      Future.successful(Redirect(routes.CannotSignUpAnotherAccountController.show()))
-                    case _ =>
-                      storeVat(vatNumber, isFromBta = false)
-                  }
-              }
-            )
-          case _ =>
-            Future.successful(
-              Redirect(routes.ResolveVatNumberController.resolve())
-            )
-        }
+      )
     }
   }
-
-  private def storeVat(vatNumber: String, isFromBta: Boolean)(implicit hc: HeaderCarrier, rc: RequestHeader): Future[Result] =
-    vatNumberOrchestrationService.storeVatNumber(vatNumber, isFromBta) map {
-      case MigratedVatNumberStored =>
-        Redirect(routes.CaptureBusinessEntityController.show())
-          .addingToSession(SessionKeys.vatNumberKey -> vatNumber)
-          .addingToSession(SessionKeys.isMigratedKey, true)
-      case NonMigratedVatNumberStored(isOverseas, isDirectDebit) if isOverseas =>
-        Redirect(routes.OverseasResolverController.resolve())
-          .addingToSession(SessionKeys.vatNumberKey -> vatNumber)
-          .addingToSession(SessionKeys.hasDirectDebitKey, isDirectDebit)
-      case NonMigratedVatNumberStored(_, isDirectDebit) =>
-        Redirect(routes.CaptureBusinessEntityController.show())
-          .addingToSession(SessionKeys.vatNumberKey -> vatNumber)
-          .addingToSession(SessionKeys.hasDirectDebitKey, isDirectDebit)
-      case ClaimedSubscription =>
-        Redirect(routes.SignUpCompleteClientController.show())
-      case Ineligible =>
-        Redirect(routes.CannotUseServiceController.show())
-      case Inhibited(migratableDates) =>
-        Redirect(routes.MigratableDatesController.show())
-          .addingToSession(SessionKeys.migratableDatesKey, migratableDates)
-      case MigrationInProgress =>
-        Redirect(routes.MigrationInProgressErrorController.show())
-      case AlreadyEnrolledOnDifferentCredential =>
-        Redirect(bta.routes.BusinessAlreadySignedUpController.show())
-      case _ =>
-        throw new InternalServerException("Unexpected response from vat number orchestration service")
-    }
 }
