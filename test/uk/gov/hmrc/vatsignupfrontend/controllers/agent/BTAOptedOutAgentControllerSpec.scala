945d87f6f8f154651a7bab1b6d37f497b235e343
Daveo-G <Daveo-G@users.noreply.github.com>
2019-11-12 11:10:10 +0000

    SAR-4794 - Added route and controller for Agent opted out flow from BTA

---
+++ test/uk/gov/hmrc/vatsignupfrontend/controllers/agent/BTAOptedOutAgentControllerSpec.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.controllers.agent
+
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.play.test.UnitSpec
+import uk.gov.hmrc.vatsignupfrontend.config.mocks.MockControllerComponents
+import uk.gov.hmrc.vatsignupfrontend.services.mocks.MockClaimSubscriptionService
+
+
+class BTAOptedOutAgentControllerSpec extends UnitSpec with GuiceOneAppPerSuite
+  with MockControllerComponents with MockClaimSubscriptionService {
+
+  object TestBTAOptedOutAgentController extends BTAOptedOutAgentController(mockControllerComponents)
+
+  val vrnFromBTA = "123456789"
+  lazy val testGetRequest = FakeRequest("GET", s"/vat-number/$vrnFromBTA")
+
+  "redirect" should {
+    "redirect to the Capture VAT number page" in {
+
+      mockAuthAdminRole()
+
+      val result = TestBTAOptedOutAgentController.redirect(vrnFromBTA)(testGetRequest)
+
+      status(result) shouldBe SEE_OTHER
+      redirectLocation(result) shouldBe Some(routes.CaptureVatNumberController.show.url)
+    }
+  }
+}
