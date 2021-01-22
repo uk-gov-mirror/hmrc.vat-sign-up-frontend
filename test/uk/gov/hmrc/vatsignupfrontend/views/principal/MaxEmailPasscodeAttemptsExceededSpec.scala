9aca043e0c2d4449f68638079c9910f793558d26
Richard Melvin <40235254+r-melvin@users.noreply.github.com>
2021-01-22 13:19:34 +0000

    SAR-6937: Create max attempts exceeded page and controller

---
+++ test/uk/gov/hmrc/vatsignupfrontend/views/principal/MaxEmailPasscodeAttemptsExceededSpec.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatsignupfrontend.views.principal
+
+import play.api.i18n.MessagesApi
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+import play.twirl.api.HtmlFormat
+import uk.gov.hmrc.vatsignupfrontend.assets.MessageLookup.{MaxEmailPasscodeAttemptsExceeded => messages}
+import uk.gov.hmrc.vatsignupfrontend.config.AppConfig
+import uk.gov.hmrc.vatsignupfrontend.views.ViewSpec
+import uk.gov.hmrc.vatsignupfrontend.views.html.principal.max_email_passcode_attempts_exceeded
+
+class MaxEmailPasscodeAttemptsExceededSpec extends ViewSpec {
+
+  lazy val appConfig: AppConfig = app.injector.instanceOf[AppConfig]
+  lazy val template: max_email_passcode_attempts_exceeded = app.injector.instanceOf[max_email_passcode_attempts_exceeded]
+  lazy val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
+  lazy val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest()
+  lazy val page: HtmlFormat.Appendable = template()(request, messagesApi.preferred(request), appConfig)
+
+
+  "The Max Email Passcode Attempts Exceeded view" should {
+
+    val testPage = TestView(
+      name = "Max Email Passcode Attempts Exceeded",
+      title = messages.title,
+      heading = messages.heading,
+      page = page,
+      haveSignOutInBanner = false
+    )
+
+    testPage.shouldHaveParaSeq(messages.line1, messages.line2)
+
+    testPage.shouldHaveALink("sign-out","sign out", appConfig.ggSignOutUrl())
+  }
+
+}
+
